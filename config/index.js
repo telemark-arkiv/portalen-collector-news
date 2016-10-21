9fc1ecfa1e7854e2cc468726e7ef64ad333a4da5
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-10-21 20:08:56 +0200

    Updates

--- config/index.js
+++ config/index.js
@@ -1,10 +1,8 @@
 'use strict'
 
-var envs = process.env
+const envs = process.env
 
-var config = {
+module.exports = {
   feedHostUrl: envs.NEWS_FEED_HOST_URL || 'https://news.portalen.no/artikler.json',
   channelId: envs.NEWS_FEED_CHANNEL_ID || 'news'
 }
-
-module.exports = config
